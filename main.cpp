5e7e3ecba1b1c5c98bebcf57da871582eccbba06
Mark Meijer <engineermarkpro@gmail.com>
2021-10-22 20:11:44 +0200

    extension support checking

--- main.cpp
+++ main.cpp
@@ -6,80 +6,95 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class AmayaEngine {
-	public:
-		void run() {
-			initWindow();
-			initVulkan();
-			mainLoop();
-			cleanup();
+public:
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
+
+private:
+	//misc stuff
+	const char* ENGINE_NAME = "AmayaEngine";
+	const char* APP_NAME = "Test App";
+
+	//glfw stuff
+	const uint32_t WIN_WIDTH = 800;
+	const uint32_t WIN_HEIGHT = 600;
+	GLFWwindow* window;
+
+	//vulkan stuff
+	VkInstance instance;
+
+	void initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, ENGINE_NAME, nullptr, nullptr);
+	}
+
+	void initVulkan() {
+		createInstance();
+	}
+
+	void createInstance() {
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = APP_NAME;
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = ENGINE_NAME;
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		createInfo.enabledLayerCount = 0;
+
+		//unsurpressable warning, please ignore
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create Vulkan instance");
 		}
 
-	private:
-		//misc stuff
-		const char* ENGINE_NAME = "AmayaEngine";
-		const char* APP_NAME = "Test App";
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-		//glfw stuff
-		const uint32_t WIN_WIDTH = 800;
-		const uint32_t WIN_HEIGHT = 600;
-		GLFWwindow* window;
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
 
-		//vulkan stuff
-		VkInstance instance;
+		std::cout << "available vulkan extensions:\n";
 
-		void initWindow() {
-			glfwInit();
-
-			glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-			window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, ENGINE_NAME, nullptr, nullptr);
+		for (const auto& extension : extensions) {
+			std::cout << '\t' << extension.extensionName << '\n';
 		}
+	}
 
-		void initVulkan() {
-			createInstance();
+	void mainLoop() {
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
 		}
+	}
 
-		void createInstance() {
-			VkApplicationInfo appInfo{};
-			appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-			appInfo.pApplicationName = APP_NAME;
-			appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
-			appInfo.pEngineName = ENGINE_NAME;
-			appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
-			appInfo.apiVersion = VK_API_VERSION_1_0;
-			
-			VkInstanceCreateInfo createInfo{};
-			createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-			createInfo.pApplicationInfo = &appInfo;
+	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
 
-			uint32_t glfwExtensionCount = 0;
-			const char** glfwExtensions;
-
-			glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-			createInfo.enabledExtensionCount = glfwExtensionCount;
-			createInfo.ppEnabledExtensionNames = glfwExtensions;
-			createInfo.enabledLayerCount = 0;
-
-			//unsurpressable warning, please ignore
-			if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-				throw std::runtime_error("failed to create Vulkan instance");
-			}
-		}
-
-		void mainLoop() {
-			while (!glfwWindowShouldClose(window)) {
-				glfwPollEvents();
-			}
-		}
-
-		void cleanup() {
-			glfwDestroyWindow(window);
-			glfwTerminate();
-		}
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
 };
 
 int main() {
